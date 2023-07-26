4e9021c9b7d6972bab023a9477a0c56f9b4ee30c
Luis briceño <luijo265@gmail.com>
2023-07-26 08:47:23 -0500

    completado leccion 10 map

--- spring-boot-reactor/src/main/java/com/webflux/reactor/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/webflux/reactor/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.webflux.reactor.springboot.reactor.app;
 
+import com.webflux.reactor.springboot.reactor.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,15 +23,37 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 		// Flux es un publicador, es decir que cambia de estado y lo siguiente
 		// es un observer que reaccione a ese cambio de estado
 //		Flux<String> nombres = Flux.just("Luis", "Dayana", "Alex", "")
-		Flux<String> nombres = Flux.just("Luis", "Dayana", "Alex")
-				.doOnNext(this::validateNombre);
+//				.map(String::toUpperCase)
+//				.doOnNext(this::validateNombre)
+//				.map(String::toLowerCase)
+		Flux<Usuario> nombres = Flux.just("Luis Cervantes",
+						"Dayana Perez",
+						"Alex Peñaranda",
+						"Oswaldo Jimenez",
+						"Dora Vallejo",
+						"Piedad Bolivar")
+				.map(this::toUpperCase)
+				.filter(usuario -> filterNombrePorLetras(usuario, "l"))
+				.doOnNext(this::validateUsuario)
+				.map(this::toLowerCase)
+				;
 //				.doOnNext(elemento -> System.out.println(elemento));
 
 		// Para suscribirnos al publisher y ver las operaciones que se ejecutan
 //		nombres.subscribe(logger::info);
 //		nombres.subscribe(logger::info, this::catchError);
+//		nombres.subscribe(
+//				this::showFromSuscribe,
+//				this::catchError,
+//				new Runnable() {
+//					@Override
+//					public void run() {
+//						logger.info("Ha finalizado ejecucion del observable con exito!");
+//					}
+//				}
+//		);
 		nombres.subscribe(
-				logger::info,
+				this::showFromSuscribeFluxUsuario,
 				this::catchError,
 				new Runnable() {
 					@Override
@@ -47,10 +70,40 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 		if (nombre.isEmpty()){
 			throw new RuntimeException("Nombre no puede estar vacio");
 		}
-		System.out.println(nombre);
+		logger.info("validateNombreFromDoOnNext {}", nombre);
+	}
+
+	private void validateUsuario(Usuario usuario) {
+		if (usuario == null){
+			throw new RuntimeException("Nombre no puede estar vacio");
+		}
+		logger.info("validateUsuarioFromDoOnNext {}", usuario);
 	}
 
 	private void catchError(Throwable e) {
 		logger.error(e.getMessage());
+	}
+
+	private void showFromSuscribe(String nombre){
+		logger.info("showFromSuscribe {}", nombre);
+	}
+
+	private void showFromSuscribeFluxUsuario(Usuario usuario){
+		logger.info("showFromSuscribeFluxUsuario {}", usuario.getNombre());
+		logger.info("showFromSuscribeFluxUsuario {}", usuario);
+	}
+
+	private Usuario toUpperCase(String nombre) {
+		String[] splitNombre = nombre.split(" ");
+		return new Usuario(splitNombre[0].toUpperCase(), splitNombre[1].toUpperCase());
+	}
+
+	private Usuario toLowerCase(Usuario usuario) {
+		usuario.setNombre(usuario.getNombre().toLowerCase());
+		return usuario;
+	}
+
+	private boolean filterNombrePorLetras(Usuario usuario, String letras) {
+		return usuario.getNombre().toLowerCase().contains(letras);
 	}
 }
